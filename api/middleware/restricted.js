36f81eb8a3a47c85acbc4a9c43d36fc51c888f7f
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-02-03 09:06:49 -0700

    middleware, tests passing

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,47 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+const { JWT_SECRET } = require('../secrets/secrets');
+const jwt = require('jsonwebtoken');
+const User = require('../users/users-model');
 
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+const restriction = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'token required' })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "token invalid" });
+    }
+    req.decodedJwt = decoded;
+    next();
+  });
 };
+
+const checkUsernameExists = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    if (!username) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
+
+    const user = await User.findBy({ username }).first();
+    if (!user) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
+
+(module.exports = restriction), checkUsernameExists, buildToken;
